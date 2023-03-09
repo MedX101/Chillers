050cb517627ef03732287addf190fef4639f700c
MedX101 <dgkhorombi@gmail.com>
2023-03-09 18:37:46 +0200

    more

--- studybud/settings.py
+++ studybud/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3#av2c6nptlbbb6^muqkchu&fe3wv&n$t2+g$v!ir-f5%doocb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['web-production-a121.up.railway.app']
+ALLOWED_HOSTS = ['web-production-a121.up.railway.app', '127.0.0.1']
 
 
 # Application definition
